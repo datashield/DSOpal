19b5530a3a5285f38e951a50dd5e0d1150b97acc
Yannick Marcon <yannick.marcon@obiba.org>
2019-02-08 12:10:21 +0100

    demo logindata added

---
+++ R/logindata.R
@@ -0,0 +1,10 @@
+#' DataSHIELD login data file
+#' 
+#' DataSHIELD login data file based on Opal demo server, with simulated data. Note that
+#' the Opal demo server is rebuilt every day and is possibly not accessible.
+#'
+#' @name logindata.opal.demo
+#' @docType data
+#' @references \url{https://opal-demo.obiba.org}
+#' @keywords data
+NULL
